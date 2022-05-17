10aa0fab3213e6646f9ed70aa4404ab798927b3c
WayneMusungu <waynemuyera17@gmail.com>
2022-05-17 15:38:34 +0300

    Create the login and sign up forms

---
+++ medscoop/__init__.py
@@ -0,0 +1,24 @@
+from crypt import methods
+from turtle import title
+
+from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from flask_bcrypt import Bcrypt
+from flask_login import LoginManager
+
+
+
+
+
+
+app = Flask(__name__)
+app.config['SECRET_KEY'] = '52b63bdd60aa1a58e3d59af3'
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///site.db'
+db = SQLAlchemy(app)
+bcrypt = Bcrypt(app)
+login_manager = LoginManager(app)
+login_manager. login_view ='login'
+login_manager.login_message_category = 'info'
+
+
+from medscoop import routes
---
+++ run.py
@@ -0,0 +1,7 @@
+from medscoop import app
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
+    
