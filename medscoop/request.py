6e1e7832570ac4732025437dbbf4cfca4c54e9a0
Ngina <Nginagitau11@outlook.com>
2022-05-18 13:10:16 +0300

    update: adds get drug api and request

--- medscoop/request.py
+++ medscoop/request.py
@@ -1,24 +1,28 @@
 import requests
+def get_drug():
+    """
+    Function to consume http request and return a Quote class instance
+    """
 
-url = "https://disease-drug-matching.p.rapidapi.com/get_disease/bethanechol"
+    url = "https://disease-drug-matching.p.rapidapi.com/get_drug/Acute%20Promyelocytic%20Leukemia"
 
-headers = {
-	"X-RapidAPI-Host": "disease-drug-matching.p.rapidapi.com",
-	"X-RapidAPI-Key": "56d9de641bmshce72ffca6d5ab55p1effe9jsn25da4cf58d27"
-}
+    headers = {
+        "X-RapidAPI-Host": "disease-drug-matching.p.rapidapi.com",
+        "X-RapidAPI-Key": "56d9de641bmshce72ffca6d5ab55p1effe9jsn25da4cf58d27"
+    }
 
-response = requests.request("GET", url, headers=headers)
+    response = requests.request("GET", url, headers=headers)
+    response.raise_for_status
+    this = response.json()
+    i = 0
+    for item in this:
+        theese = this[i]['drug']
+        i += 1
+        if theese == theese:
+            break
+        else:   
+            print(theese)
+    return theese
+get_drug()
 
-print(response.text)
 
-
-# import requests
-
-# url = 'https://dailymed.nlm.nih.gov/dailymed/services/v2/drugnames.json?name_type=both&pagesize=5&page=3
-# '
-
-# def get_drugs():
-#     """
-#     Function to consume http request and return a Quote class instance
-#     """
-#     response = requests.get(url).json()
